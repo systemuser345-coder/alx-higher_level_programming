f50eb0b46a5b877c835c517968545c5feadb9a95
systemuser345-coder <segundahunsi3@gmail.com>
2024-03-21 11:15:22 -0700

    python-data_structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def divisble_by_2(my_list[]):
 
-    new_list = my_list.copy()
+def divisible_by_2(my_list=[]):
+    new_l = my_list.copy()
     for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list[i] = 1
+            new_l[i] = 1
         else:
-            new_list[i] = 0
-    return new_list
+            new_l[i] = 0
+    return new_l
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    """Finds the biggest integer of a list."""
 
     if not my_list:
         return None
 
-    max_num = my_list[0]
-    for x in my_list:
-        if x > max_num:
-            max_num = x
-    return max_num
+    max_i = my_list[0]
+    for i in my_list:
+        if i > max_i:
+            max_i = i
+    return max_i
